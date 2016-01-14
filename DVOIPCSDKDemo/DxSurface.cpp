0da277702eb4add5904aecc0a83395c0c2e866e7
leither <leither908@gmail.com>
2016-01-14 23:09:34 +0800

    2016.01.14 delete a demo @home

--- DVOIPCSDKDemo/DxSurface.cpp
+++
@@ -1,4 +0,0 @@
-#include "DxSurface.h"
-WndSurfaceMap	CDxSurface::m_WndSurfaceMap;
-//WNDPROC	CDxSurface::m_pOldWndProc = NULL;
-CriticalSectionPtr CDxSurface::m_WndSurfaceMapcs = shared_ptr<CriticalSectionWrap>(new CriticalSectionWrap());
--- DVOIPCSDKDemo/VideoDecode.cpp
+++
@@ -1,5 +0,0 @@
-
-#include "VideoDecode.h"
-CFullScreen CVideoPlayer::m_ArrayFullScreen[_Max_Monitors];
-int		CVideoPlayer::	m_nFullScreenInUsing = 0;// 正在使用的全屏显示器数量
-CRITICAL_SECTION	CVideoPlayer::m_csArrayFullScreen;
